27f086e8d4d5811e0bb4e7d40d5f2f1e00aded45
maxkrieg <maxwell.krieg@gmail.com>
2015-05-31 10:01:20 -0400

    Added basic html and css styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 
 
-$('#leftbar').hide();
+$('#leftbar-create-event').hide();
 
-$('#add-event').click(function(){
-  $('#leftbar').show(500);
-})
+$('#add-event-btn').click(function(){
+  $('#leftbar-itineraries-list').hide();
+  $('#leftbar-create-event').show();
+
+});
+
+$('#submit-btn').click(function(){
+  $('#leftbar-create-event').hide();
+  $('#leftbar-itineraries-list').show();
+});
+
